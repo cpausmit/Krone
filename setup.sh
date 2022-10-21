8996da52a811b393374add73467840211600c5cd
Christoph Paus <paus@mit.edu>
2022-10-21 08:08:04 -0400

    New setup.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 alias c19='c19.py --combine=7 --relative --delta --tmin=2020-04-01'
-export KRONE_BASE=/home/paus/c19/Krone
+export KRONE_BASE=/home/paus/Work/Krone
 export PYTHONPATH="${PYTHONPATH}:${KRONE_BASE}/python"
 export PATH="$PATH:$KRONE_BASE"
